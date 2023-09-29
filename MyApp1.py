bfb1201fa37089b5d57f7c22043fc37ce8047443
633230015 <139665315+Waris015@users.noreply.github.com>
2023-09-29 13:34:47 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.title("Index Price Prediction")
 st.header("Index Price Prediction from NPRU")
 
-df=pd.read_csv('./data/stock_index_price.csv')
+df=pd.read_csv("./data/stock_index_price.csv")
 st.write(df.head(10))
 
 st.header("Index Price Prediction Chart")
