fc564d881ed7bb32ee800167caafbe1b6b710226
633230015 <139665315+Waris015@users.noreply.github.com>
2023-09-29 13:37:24 +0700

    Update MyApp1.py

--- MyApp1.py
+++ MyApp1.py
@@ -8,4 +8,4 @@ df=pd.read_csv("./data/stock_index_price.csv")
 st.write(df.head(10))
 
 st.header("Index Price Prediction Chart")
-st.line_char(df)
+st.line_char(df, x="interest_rate",y=["unemployment_rate","stock_index_price"])
