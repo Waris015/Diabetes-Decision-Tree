33b6fc2023811b4d001954805f799606f4b853d1
633230015 <139665315+Waris015@users.noreply.github.com>
2023-09-29 13:32:39 +0700

    .commit

--- MyApp1.py
+++ MyApp1.py
@@ -7,5 +7,5 @@ st.header("Index Price Prediction from NPRU")
 df=pd.read_csv('./data/stock_index_pice.csv')
 st.write(df.head(10))
 
-st.write("Index Price Prediction Chart")
+st.header("Index Price Prediction Chart")
 st.line_char(df)
