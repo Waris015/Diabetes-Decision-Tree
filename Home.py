77f6e614a4112c8df39cb3d14d8c04fdb30c73fe
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 21:16:01 +0700

    c

--- Home.py
+++ Home.py
@@ -1,25 +1,5 @@
-import pandas as pd
 import streamlit as st
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score,mean_squared_error
 
-st.title("Index Price Prediction")
-st.header("Index Price Prediction from NPRU")
 
-df=pd.read_csv("./data/Diabetes_Classification.csv")
-st.write(df.head(10))
+st.title("การพยากรณ์การเป็นโรคเบาหวาน")
 
-x1=st.number_input("กรุณาป้อนข้อมูล interest_rate:")
-x2=st.number_input("กรุณาป้อนข้อมูล unemployment_rate:")
-
-if st.button("พยากรณ์ข้อมูล"):
-    x_input=[[x1,x2]]
-    y_predict=modelRegress.predict(pf.fit_transform(x_input))
-    st.write(y_predict)
-    st.button("ไม่พยากรณ์ข้อมูล")
-else:
-    st.button("ไม่พยากรณ์ข้อมูล")
