e6ea13f9f8804fce56128d65412780ac228d214e
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 21:52:40 +0700

    c

---
+++ Diabetes.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import streamlit as st
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
+
+st.title("Index Price Prediction")
+st.header("Index Price Prediction from NPRU")
+
+df=pd.read_csv("./data/Diabetes_Classification.csv")
+st.write(df.head(10))
+
+x1=st.number_input("กรุณาป้อนข้อมูล อายุ:")
+x2=st.text_input("กรุณาป้อนข้อมูล unemployment_rate:")
+
+
+if st.button("พยากรณ์ข้อมูล"):
+    x_input=[[x1,x2,]]
+    y_predict=modelRegress.predict(pf.fit_transform(x_input))
+    st.write(y_predict)
+    st.button("ไม่พยากรณ์ข้อมูล")
+else:
+    st.button("ไม่พยากรณ์ข้อมูล")
