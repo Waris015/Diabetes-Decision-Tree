ca448d69cf3c3eeeca972023571ce2d0ae0e8c49
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 22:43:05 +0700

    Update Diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -1,29 +1,42 @@
 import pandas as pd
 import streamlit as st
-import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error
 
 st.title("Index Price Prediction")
 st.header("Index Price Prediction from NPRU")
 
-df=pd.read_csv("./data/Diabetes_Classification.csv")
-st.write(df.head(10))
+# อ่านข้อมูลจากไฟล์ CSV
+df = pd.read_csv("./data/Diabetes_Classification.csv")
+st.write(df.head(10)
 
-x1=st.number_input("กรุณาป้อนข้อมูล อายุ:")
-x2=st.number_input("กรุณาป้อนข้อมูล เพศ (เพศชาย 1, เพศหญิง 2):")
-x3=st.number_input("กรุณาป้อนข้อมูล BMI:")
-x4=st.number_input("กรุณาป้อนข้อมูล ความดันโลหิต:")
-x5=st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลในเลือด:")
-x6=st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลสะสม:")
+x1 = st.number_input("กรุณาป้อนข้อมูล อายุ:")
+x2 = st.number_input("กรุณาป้อนข้อมูล เพศ (เพศชาย 1, เพศหญิง 2):")
+x3 = st.number_input("กรุณาป้อนข้อมูล BMI:")
+x4 = st.number_input("กรุณาป้อนข้อมูล ความดันโลหิต:")
+x5 = st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลในเลือด:")
+x6 = st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลสะสม:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    x_input=[[x1,x2,x3,x4,x5,x6]]
-    y_predict=modelRegress.predict(pf.fit_transform(x_input))
-    st.write(y_predict)
-    st.button("ไม่พยากรณ์ข้อมูล")
+    # สร้างโมเดล Linear Regression
+    modelRegress = LinearRegression()
+
+    # ทำการตรวจสอบข้อมูลและเตรียมข้อมูลสำหรับการพยากรณ์
+    X = df[['อายุ', 'เพศ', 'BMI', 'ความดันโลหิต', 'ค่าน้ำตาลในเลือด', 'ค่าน้ำตาลสะสม']]
+    y = df['target']
+
+    # แบ่งข้อมูลเป็นชุดข้อมูลการฝึกอบรมและการทดสอบ
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # ฝึกโมเดล
+    modelRegress.fit(X_train, y_train)
+
+    # พยากรณ์ค่า
+    x_input = [[x1, x2, x3, x4, x5, x6]]
+    y_predict = modelRegress.predict(x_input)
+    st.write("ผลลัพธ์การพยากรณ์:", y_predict[0])
 else:
     st.button("ไม่พยากรณ์ข้อมูล")
