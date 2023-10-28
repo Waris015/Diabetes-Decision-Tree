7e86cbb291ca6ad699376298c9c620340eaa2376
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 20:29:37 +0700

    c

--- Ford.py
+++ Ford.py
@@ -13,10 +13,16 @@ st.header("Index Price Prediction from NPRU")
 df=pd.read_csv("./data/Diabetes_Classification.csv")
 st.write(df.head(10))
 
-st.header("Index Price Prediction Chart")
-st.line_chart(df, x="interest_rate",y=["unemployment_rate","Diabetes_Classification"])
 
+x=df[['interest_rate','unemployment_rate']]
+y=df['stock_index_price']
+pf=PolynomialFeatures(degree=3)
+x_poly=pf.fit_transform(x)
 
+x_train,x_test,y_train,y_test =train_test_split(x_poly,y,random_state=0)
+
+modelRegress=LinearRegression()
+modelRegress.fit(x_train,y_train)
 x1=st.number_input("กรุณาป้อนข้อมูล interest_rate:")
 x2=st.number_input("กรุณาป้อนข้อมูล unemployment_rate:")
 
