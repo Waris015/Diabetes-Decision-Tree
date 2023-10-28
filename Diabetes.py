468d9ea1e5eaae0ef914d53c677097a6d62aecaa
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 21:56:43 +0700

    Update Diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -1,26 +1,46 @@
 import pandas as pd
 import streamlit as st
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,mean_squared_error
 
-st.title("Index Price Prediction")
-st.header("Index Price Prediction from NPRU")
+st.title("hart axtex")
+st.header("Hart NPRU")
 
-df=pd.read_csv("./data/Diabetes_Classification.csv")
+df=pd.read_csv('./data/Diabetes_Classification.csv')
 st.write(df.head(10))
 
-x1=st.number_input("กรุณาป้อนข้อมูล อายุ:")
-x2=st.text_input("กรุณาป้อนข้อมูล unemployment_rate:")
-x3=st.text_input("กรุณาป้อนข้อมูล unemployment_rate:")
+#st.line_chart(df)
+#st.line_chart(df, x="interest_rate", y="unemployment_rate", color="stock_index_price")
 
+x=df.iloc[:, 0:9]
+y=y = df['Diagnosis']
+
+x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=0.3,random_state=1)
+
+sc = StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.transform(x_test)
+
+DTmodel = DecisionTreeClassifier(criterion='gini')
+DTmodel.fit(x_train, y_train)
+
+x1=st.number_input("กรุณาป้อนข้อมูล อายุ:")
+x2=st.number_input("กรุณาป้อนข้อมูล เพศ (0 สำหรับผู้หญิง, 1 สำหรับผู้ชาย):")
+x3=st.number_input("กรุณาป้อนข้อมูล อัตราการเต้นของหัวใจ:")
+x4=st.number_input("กรุณาป้อนข้อมูล ความดันสูง:")
+x5=st.number_input("กรุณาป้อนข้อมูล ความดันต่ำ:")
+x6=st.number_input("กรุณาป้อนข้อมูล น้ำตาลในเลือด:")
+x7=st.number_input("กรุณาป้อนข้อมูล โรคกล้ามเนื้อ (CK-MB):")
+x8=st.number_input("กรุณาป้อนข้อมูล โทรโปนิน .:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    x_input=[[x1,x2,x3]]
-    y_predict=modelRegress.predict(pf.fit_transform(x_input))
+    x_input=[[x1,x2,x3,x4,x5,x6,x7,x8]]
+    y_predict=DTmodel.predict(x_input)
     st.write(y_predict)
     st.button("ไม่พยากรณ์ข้อมูล")
 else:
