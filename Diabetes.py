e4a4e15436556b7c006e1bab85c89082c36e3cb9
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 23:13:12 +0700

    Update Diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -1,39 +1,29 @@
 import pandas as pd
 import streamlit as st
+import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
 
-st.title("Diabetes Diagnosis Prediction")
-st.header("Predicting Diabetes Diagnosis from Health Data")
+st.title("Index Price Prediction")
+st.header("Index Price Prediction from NPRU")
 
-# อ่านข้อมูลจากไฟล์ CSV
-df = pd.read_csv("Diabetes_Classification.csv")
+df=pd.read_csv("./data/Diabetes_Classification.csv")
 st.write(df.head(10))
 
-x1 = st.number_input("กรุณาป้อนข้อมูลอายุ:")
-x2 = st.number_input("กรุณาป้อนข้อมูลเพศ (เพศชาย 1, เพศหญิง 2):")
-x3 = st.number_input("กรุณาป้อนข้อมูล BMI:")
-x4 = st.number_input("กรุณาป้อนข้อมูลความดันโลหิต:")
-x5 = st.number_input("กรุณาป้อนข้อมูลค่าน้ำตาลในเลือด:")
-x6 = st.number_input("กรุณาป้อนข้อมูลค่า HbA1c:")
+x1=st.number_input("กรุณาป้อนข้อมูล อายุ:")
+x2=st.number_input("กรุณาป้อนข้อมูล เพศ (เพศชาย 1, เพศหญิง 2):")
+x3=st.number_input("กรุณาป้อนข้อมูล BMI:")
+x4=st.number_input("กรุณาป้อนข้อมูล ความดันโลหิต:")
+x5=st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลในเลือด:")
+x6=st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลสะสม:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    # เตรียมข้อมูล
-    X = df[['Age', 'Gender', 'BMI', 'Pressure', 'FBS', 'HbA1c']]
-    y = df['Diagnosis']  # คอลัมน์ที่เราจะพยากรณ์
-
-    # สร้างโมเดล Linear Regression
-    model = LinearRegression()
-
-    # ฝึกโมเดล
-    model.fit(X, y)
-
-    # ทำการพยากรณ์ผล
-    x_input = [[x1, x2, x3, x4, x5, x6]]
-    diagnosis = model.predict(x_input)[0]
-
-    if diagnosis == "Yes":
-        st.write("ผลลัพธ์การพยากรณ์: ผู้ป่วยเบาหวาน")
-    else:
-        st.write("ผลลัพธ์การพยากรณ์: ไม่เป็นผู้ป่วยเบาหวาน")
-
-# เพิ่มเงื่อนไขอื่น ๆ ที่คุณต้องการแสดงผลหรือกระทำต่อไป
+    x_input=[[x1,x2,x3,x4,x5,x6]]
+    y_predict=modelRegress.predict(pf.fit_transform(x_input))
+    st.write(y_predict)
+    st.button("ไม่พยากรณ์ข้อมูล")
+else:
+    st.button("ไม่พยากรณ์ข้อมูล")
