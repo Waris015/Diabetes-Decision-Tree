d6cb7196e3c40e7b2c7d4909cc9d6987e8d060f4
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-28 21:07:24 +0700

    Update _1_page.py

--- pages/_1_page.py
+++ pages/_1_page.py
@@ -3,4 +3,4 @@ import streamlit as st
 
 st.title("การพยากรณ์การเป็นโรคเบาหวาน")
 
-st.write("You have entered", st.session_state["my_input"])
+st.write("กดยนเยดกเยหดก", st.session_state["my_input"])
