f2d0465c3d11b3ebc033eef09c3c8d98b2c08d5f
Waris015 <139665315+Waris015@users.noreply.github.com>
2023-10-29 00:06:48 +0700

    Update Diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -1,39 +1,45 @@
 import pandas as pd
 import streamlit as st
-from sklearn.linear_model import LinearRegression
+import seaborn as sns
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,mean_squared_error
 
-st.title("Index Price Prediction")
-st.header("Index Price Prediction from NPRU")
+st.title("hart axtex")
+st.header("Hart NPRU")
 
-# อ่านข้อมูลจากไฟล์ CSV
-df = pd.read_csv("./data/Diabetes Classification.csv")
+df=pd.read_csv('./data/Diabetes Classification.csv')
 st.write(df.head(10))
 
-x1 = st.number_input("กรุณาป้อนข้อมูล Age:")
-x2 = st.number_input("กรุณาป้อนข้อมูล Gender (เพศชาย 1, เพศหญิง 2):")
-x3 = st.number_input("กรุณาป้อนข้อมูล BMI:")
-x4 = st.number_input("กรุณาป้อนข้อมูล Pressure:")
-x5 = st.number_input("กรุณาป้อนข้อมูล FBS:")
-x6 = st.number_input("กรุณาป้อนข้อมูล HbA1c:")
+#st.line_chart(df)
+#st.line_chart(df, x="interest_rate", y="unemployment_rate", color="stock_index_price")
+
+x=df.iloc[:, 0:8]
+y=y = df['diagnosis']
+
+x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=0.3,random_state=1)
+
+sc = StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.transform(x_test)
+
+DTmodel = DecisionTreeClassifier(criterion='gini')
+DTmodel.fit(x_train, y_train)
+
+x1=st.number_input("กรุณาป้อนข้อมูล อายุ:")
+x2=st.number_input("กรุณาป้อนข้อมูล เพศ (เพศชาย 1, เพศหญิง 2):")
+x3=st.number_input("กรุณาป้อนข้อมูล BMI:")
+x4=st.number_input("กรุณาป้อนข้อมูล ความดันโลหิต:")
+x5=st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลในเลือด:")
+x6=st.number_input("กรุณาป้อนข้อมูล ค่าน้ำตาลสะสม:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    # สร้างโมเดล Linear Regression
-    modelRegress = LinearRegression()
-
-    # ทำการตรวจสอบข้อมูลและเตรียมข้อมูลสำหรับการพยากรณ์
-    X = df[['Age', 'Gender', 'BMI', 'Pressure', 'FBS', 'HbA1c']]
-    y = df['target']
-
-    # แบ่งข้อมูลเป็นชุดข้อมูลการฝึกอบรมและการทดสอบ
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # ฝึกโมเดล
-    modelRegress.fit(X_train, y_train)
-
-    # พยากรณ์ค่า
-    x_input = [[x1, x2, x3, x4, x5, x6]]
-    y_predict = modelRegress.predict(x_input)
-    st.write("ผลลัพธ์การพยากรณ์:", y_predict[0])
+    x_input=[[x1,x2,x3,x4,x5,x6,]]
+    y_predict=DTmodel.predict(x_input)
+    st.write(y_predict)
+    st.button("ไม่พยากรณ์ข้อมูล")
 else:
     st.button("ไม่พยากรณ์ข้อมูล")
