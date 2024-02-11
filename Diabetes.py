74eb9515d89ad1d149d1c359400e6ddd7581170b
Waris015 <139665315+Waris015@users.noreply.github.com>
2024-02-11 11:57:40 +0700

    Update Diabetes.py

--- Diabetes.py
+++ Diabetes.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # ข้อความหัวข้อ
 with st.container():
-    st.title("แบบทดสอบ")
+    st.form("แบบทดสอบ")
     st.markdown("""
         ---
         """)
@@ -12,7 +12,7 @@ name = st.text_input("ชื่อ-นามสกุล ผู้ทำข้�
 
 # ข้อ 1
 q1_options = ["will", "can", "is", "are"]
-q1_answer = st.radio("She ___ ", q1_options, index=0)
+q1_answer = st.radio("She ___ run ", q1_options, index=0)
 
 # ปุ่มส่งคำตอบ
 if st.button("ส่งคำตอบ"):
